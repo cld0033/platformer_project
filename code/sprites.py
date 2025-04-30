f385a088b3723da053d31f415e754b5f2e91756e
Chester Davidson <cld0033@auburn.edu>
2025-04-30 14:46:48 -0500

    created platformer game

---
+++ code/sprites.py
@@ -0,0 +1,47 @@
+from settings import *
+
+
+class Sprite(pygame.sprite.Sprite):
+  def __init__(self, position, surf, groups):
+    super().__init__(groups)
+    self.image = surf
+    self.rect = self.image.get_frect(topleft=position)
+
+class Player(Sprite):
+  def __init__(self, position, groups, collision_sprites):
+    surf = pygame.Surface((40, 80))
+    super().__init__(position, surf, groups)
+
+    #movement and collision
+    self.direction = pygame.Vector2()
+    self.collision_sprites = collision_sprites
+    self.speed = 400
+    self.gravity = 50
+
+  def input(self):
+    keys = pygame.key.get_pressed()
+    self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+
+  def move(self, dt):
+    #horizontal
+    self.rect.x += self.direction.x * self.speed * dt
+    self.collision('horizontal')
+
+    #vertical
+    self.direction.y += self.gravity * dt
+    self.rect.y += self.direction.y
+    self.collision('vertical')
+
+  def collision(self, direction):
+    for sprite in self.collision_sprites:
+      if sprite.rect.colliderect(self.rect):
+        if direction =='horizontal':
+          if self.direction.x > 0: self.rect.right = sprite.rect.left
+          if self.direction.x < 0: self.rect.left = sprite.rect.right
+        if direction == 'vertical':
+          if self.direction.y > 0: self.rect.bottom = sprite.rect.top
+          if self.direction.y < 0: self.rect.top = sprite.rect.bottom
+
+  def update(self, dt):
+    self.input()
+    self.move(dt)
